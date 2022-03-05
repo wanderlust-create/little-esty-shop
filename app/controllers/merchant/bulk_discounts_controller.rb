ce116361494d48f7b5644b131c2aefa330281d08
Becky Nisttahuz <benistta@hotmail.com>
2022-03-04 21:54:58 -0700

    Feat: Create method for destroy

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -7,6 +7,18 @@ class Merchant::BulkDiscountsController < ApplicationController
    @bulk_discount = BulkDiscount.find(params[:id])
   end
 
+  def new
+    @discount = BulkDiscount.new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    bulk_discount = params[:bulk_discount]
+    @merchant = Merchant.find(params[:merchant_id])
+    BulkDiscount.create(percentage_discount: bulk_discount[:percentage_discount], quantity_threshold: bulk_discount[:quantity_threshold], merchant: @merchant)
+    redirect_to merchant_bulk_discounts_path(@merchant)
+  end
+
   def destroy
     @merchant = Merchant.find_by(id: params[:merchant_id])
     BulkDiscount.find(params[:id]).destroy
