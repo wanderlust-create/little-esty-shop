be70b3b3fc459bda2cc39d97c65de8bb6eb8df49
Becky Nisttahuz <benistta@hotmail.com>
2022-03-04 20:49:31 -0700

    Feat: Add controller delete for bulk discounts controller

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -4,5 +4,12 @@ class Merchant::BulkDiscountsController < ApplicationController
   end
 
   def show
+   @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  def destroy
+    @merchant = Merchant.find_by(id: params[:merchant_id])
+    BulkDiscount.find(params[:id]).destroy
+    redirect_to merchant_bulk_discounts_path(@merchant.id)
   end
 end
